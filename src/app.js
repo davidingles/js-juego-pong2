c7960c7473f03fdaddd7675db133dd5a3c7c4a34
David Inglés <davidingleslopez@gmail.com>
2023-11-16 23:35:01 +0100

    el movimiento de las barras funciona perfectamente

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 import juego from './objetoJuego.js'
 let { tiempo, alturaBarra, anchuraMesa, alturaMesa, direccion, btn, acelera, PARAR, player1, player2, KEYA, KEYQ, KEYO, KEYL, setInicio } = juego
 
+
+
 function GAME() {
 
   function start() {
@@ -11,6 +13,7 @@ function GAME() {
 
   function stop() {
     clearInterval(PARAR)
+    document.body.style.backgroundColor = 'red'
   }
   function acelerar() {
     tiempo = 20
@@ -21,26 +24,47 @@ function GAME() {
 
   function play() {
     console.log('object');
+    moveBar()
+    // moveBall() //todo: pendiente de implementar
   }
-
   document.onkeydown = function (e) {
-    console.log(e);
     switch (e.code) {
       case KEYA:
       case KEYQ:
-        player1.keyPress = true;
-        player1.keyCode = e.code;
+        player1.keyPress = true
+        player1.keyCode = e.code
+        break
       case KEYO:
       case KEYL:
-        player2.keyPress = true;
-        player2.keyCode = e.code;
-        break;
+        player2.keyPress = true
+        player2.keyCode = e.code
+        break
     }
   }
+  document.onkeyup = function (e) {
+    if (e.code === KEYA || e.code === KEYQ) {
+      player1.keyPress = false
+      console.log('he soltado KEYA');
+    }
+    if (e.code === KEYO || e.code === KEYL) { player2.keyPress = false }
+  }
 
-  console.log('object');
+  function moveBar() {
 
+    if (player1.keyPress) {
+      if (player1.keyCode === KEYA) { barra1.style.top = (barra1.offsetTop + 5) + 'px' }
+      else if (player1.keyCode === KEYQ) { barra1.style.top = (barra1.offsetTop - 5) + 'px' }
+    }
+    if (player2.keyPress) {
+      if (player2.keyCode === KEYO) {
+        barra2.style.top = (barra2.offsetTop - 5) + 'px'
+      }
+      else if (player2.keyCode === KEYL) {
+        barra2.style.top = (barra2.offsetTop + 5) + 'px'
+      }
 
+    }
+  }
 
 
 
